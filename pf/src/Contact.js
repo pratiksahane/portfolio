e84484f9e8c4a2c88d6c324af2d766c6ba94686b
Pratik <pratiksahane351@gmail,com>
2025-04-05 20:46:52 +0530

    Minor Changes

--- pf/src/Contact.js
+++ pf/src/Contact.js
@@ -13,7 +13,12 @@ const Contact=()=>{
       <a href="https://www.linkedin.com/in/pratik-sahane-162636294/" target="_blank" rel="noopener noreferrer">
         <i className="fab fa-linkedin-in"></i>
       </a>
+      <p><i className="fas fa-phone"></i> 9373349858</p>
+      <p><i className="fas fa-envelope"></i> pratiksahane351@gmail.com</p>
+      <p><i className="fas fa-copyright"></i> Copyrights attached</p>
+
     </div>
+
     </>);
 
 }
